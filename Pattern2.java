dc15febedea56f5a482732100c386a14bc53afae
AnjanKumarS <anjankumars65432@gmail.com>
2024-08-24 12:51:56 +0530

    Patter programs

--- Pattern2.java
+++ Pattern2.java
@@ -1,20 +1,19 @@
-public class Pattern1 {
+public class Pattern2 {
     public static void main(String[] args) {
         int n = 5;
-
         for(int r=1;r<=n;r++)
         {
-            for(int c=1;c<=n;c++)
+            for(int c=1;c<=n*2-1;c++)
             {
-                if (r >= c) {
+                if (r + c >= n + 1 && c - r <= n - 1) {
                     System.out.print("* ");
                 }
                 else
                 {
-                    System.out.println("  ");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
         }
-    }    
+    }
 }
